922dd9899bb93726a076c83ef6114ea9609d57ca
Martin Akula <martin.akula@gmail.com>
2017-10-22 15:48:39 +0300

    [UPDATE]Added testing of the links

--- test_run.py
+++ test_run.py
@@ -5,11 +5,16 @@ from app import app
 
 
 class TestApp(unittest.TestCase):
- """This tests the initialisation"""
+ """This tests the initialization"""
 def setUp(self):
         self.app.config['DEBUG']=False
-def test_links():                
-    pass
+def test_links(self):                
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
 
 if __name__ == '__main__':
        unittest.main()
