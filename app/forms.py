be9ba595cdef020dd097024928c4fe6141508119
Martin Akula <martin.akula@gmail.com>
2017-10-23 09:09:36 +0300

    [UPADATE]Added Registration class

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import Email, InputRequired
+from flask_login import UserMixin
+
+
+class RegisterForm(FlaskForm):
+    regfirstname = StringField('regfirstname', validators=[InputRequired()])
+    reglastname = StringField('reglastname', validators=[InputRequired()])
+    regemail = StringField('regemail', validators=[InputRequired(), Email('Invalid Email')])
+    regusername = StringField('regusername', validators=[InputRequired()])
+    regpassword = PasswordField('regpassword', validators=[InputRequired()])
