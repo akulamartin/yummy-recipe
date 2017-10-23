b15a05efb96cddf09fa344272957b436fee3be66
Martin Akula <martin.akula@gmail.com>
2017-10-23 08:55:53 +0300

    [UPDATE]Addde GET,POST methods

--- app/views.py
+++ app/views.py
@@ -4,19 +4,23 @@ from flask import render_template
 
 from app import app
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-@app.route('/ForgotPassword')
+
+@app.route('/ForgotPassword', methods=['GET', 'POST'])
 def about():
     return render_template("ForgotPassword.html")
 
-@app.route('/Categories')
+
+@app.route('/Categories', methods=['GET', 'POST'])
 def Categories():
     return render_template("Categories.html")
 
-@app.route('/UserRegistration')
+
+@app.route('/UserRegistration', methods=['GET', 'POST'])
 def UserRegistration():
     return render_template("UserRegistration.html")
 
